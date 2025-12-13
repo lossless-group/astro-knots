979c1ce1ba01a9e5960fdaf4cdba897e01b5e756
mpstaton <mpstaton@gmail.com>
2025-12-13 16:24:52 +0100

    new(packages, releases): add new packages and releases of client sites
     On branch master
     Your branch is ahead of 'origin/master' by 2 commits.
       (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   packages/astro/package.json
    	new file:   packages/astro/src/components/articles/MarkdownArticleOnPage.astro
    	new file:   packages/astro/src/components/markdown/AstroMarkdown.astro
    	new file:   packages/astro/src/components/markdown/Callout.astro
    	new file:   packages/astro/src/components/markdown/CodeBlock.astro
    	modified:   packages/astro/src/index.ts
    	new file:   packages/astro/src/layouts/MarkdownArticle.astro
    	modified:   sites/dark-matter
    	modified:   sites/hypernova-site

--- packages/astro/src/index.ts
+++ packages/astro/src/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from "./components/Button.astro";
+export { default as MarkdownArticle } from "./layouts/MarkdownArticle.astro";
+export { default as MarkdownArticleOnPage } from "./components/articles/MarkdownArticleOnPage.astro";
+export { default as AstroMarkdown } from "./components/markdown/AstroMarkdown.astro";
